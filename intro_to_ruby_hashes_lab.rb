36dce8d611b584d867b3de81097cece64e9f9910
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-02 20:30:53 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,6 +34,18 @@ def monopoly_with_third_tier
 			pieces:4,
 			names:
 			{
+				reading_railroad:
+				{
+				}
+				pennsylvania_railroad:
+				{
+				}
+				b_and_o_railroad:
+				{
+				}
+				shortline:
+				{
+				}
 			},
 			rent_in_dollars:
 			{
